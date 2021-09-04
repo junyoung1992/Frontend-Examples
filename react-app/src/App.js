054a449818cb20d8d4e3bbc39f2b928783e4cf03
Kim Junyoung <orsay0827@gmail.com>
2021-09-05 00:25:36 +0900

    Study React: CRUD

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import TOC from './components/TOC';
 import Control from './components/Control';
 import ReadContent from './components/ReadContent';
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import './App.css';
 
 class App extends Component {
@@ -17,32 +18,34 @@ class App extends Component {
       welcome: {title: 'Welcome', desc: 'Hello React!!!'},
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is HyperText Markup Language.'},
-        {id: 2, title: 'CSS', desc: 'CSS is for design'},
-        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+        {id: 2, title: 'CSS', desc: 'CSS is for design.'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive.'}
       ]
     }
   }
 
-  render() {
-    console.log('App render');
+  getReadContent() {
+    for(var i = 0; i < this.state.contents.length; i++) {
+      var data = this.state.contents[i];
+
+      if(data.id === this.state.selected_content_id) {
+        return data;
+      }
+    }
+  }
+
+  getContent() {
     var _title, _desc, _article = null;
     
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+
       _article = <ReadContent title={_title} desc={_desc} />
     } else if(this.state.mode === 'read') {
-      for(var i = 0; i < this.state.contents.length; i++) {
-        var data = this.state.contents[i];
+      let _content = this.getReadContent();
 
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-      }
-
-      _article = <ReadContent title={_title} desc={_desc} />
+      _article = <ReadContent title={_content.title} desc={_content.desc} />
     } else if(this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         // add content to this.state.content
@@ -62,11 +65,36 @@ class App extends Component {
         );
 
         this.setState({
+          mode: "read",
+          selected_content_id: this.max_content_id,
+          contents: _contents,
+        });
+      }.bind(this)}/>
+    } else if(this.state.mode === 'update') {
+      let _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc) {
+        var _contents = Array.from(this.state.contents);
+
+        for(var i = 0; i< _contents.length; i++) {
+          if(_contents[i].id === _id) {
+            _contents[i] = {id: _id, title: _title, desc: _desc};
+            break;
+          }
+        }
+
+        this.setState({
+          mode: "read",
           contents: _contents,
         });
       }.bind(this)}/>
     }
 
+    return _article;
+  }
+
+  render() {
+    console.log('App render');
+    
     return (  
       <div className="App">
         <Subject
@@ -88,13 +116,33 @@ class App extends Component {
 
         <Control
           onChangeMode={function(_mode) {
-            this.setState({
-              mode: _mode,
-            })
+            if(_mode === 'delete') {
+              if(window.confirm('Really?')) {
+                var _contents = Array.from(this.state.contents);
+
+                for(var i = 0; i < _contents.length; i++) {
+                  if(_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                }
+                
+                this.setState({
+                  mode: 'welcome',
+                  contents: _contents
+                })
+
+                alert("Delete!");
+              }
+            } else {
+              this.setState({
+                mode: _mode,
+              })
+            }
           }.bind(this)}
         />
 
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
