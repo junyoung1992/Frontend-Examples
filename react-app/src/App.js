d44df98b79c4ed6a01624cd859680b065bb29ae2
Kim Junyoung <orsay0827@gmail.com>
2021-09-04 14:22:06 +0900

    Study React: CRUD - Create

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Subject from './components/Subject';
 import TOC from './components/TOC';
-import Content from './components/Content'
+import Control from './components/Control';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
+      selected_content_id: 2,
       subject: {title: 'WEB', sub: 'World Wide Web!'},
       welcome: {title: 'Welcome', desc: 'Hello React!!!'},
       contents: [
@@ -21,36 +25,76 @@ class App extends Component {
 
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode == 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      _article = <ReadContent title={_title} desc={_desc} />
+    } else if(this.state.mode === 'read') {
+      for(var i = 0; i < this.state.contents.length; i++) {
+        var data = this.state.contents[i];
+
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+      }
+
+      _article = <ReadContent title={_title} desc={_desc} />
+    } else if(this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        // add content to this.state.content
+        this.max_content_id++;
+
+        // push를 사용하면 this.state.contents 원본을 변환하므로,
+        // shouldComponentUpdate 에서 newProps.data와 this.props.data 가 같아짐 -> 튜닝이 어려움
+        // this.state.contents.push({id: this.max_content_id, title: _title, desc: _desc});
+        
+        // Array.from 으로 복제한 다음 push 하거나 concat 을 사용하면 됨
+        //    - 배열: var newArray = Array.from(originalArray);
+        //    - 객체: var newObject = Object.assign({}, originalObject);
+        // var newContents = Array.from(this.state.contents);
+        // newContents.push({id: this.max_content_id, title: _title, desc: _desc});
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        );
+
+        this.setState({
+          contents: _contents,
+        });
+      }.bind(this)}/>
     }
 
     return (  
       <div className="App">
-        {/* <Subject
+        <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub} /> */}
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({mode: 'welcome'});
+          }.bind(this)}
+        />
 
-        <header>
-          <h1><a href="/" onClick={function(e) {
-            console.log(e);
-            e.preventDefault();
-            // this.state.mode = 'welcome';
+        <TOC
+          data={this.state.contents}
+          onChangePage={function(_id) {
             this.setState({
-              mode: 'welcome'
-            });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
+              mode: 'read',
+              selected_content_id: Number(_id)});
+          }.bind(this)}
+        />
 
-        <TOC data={this.state.contents} />
-        <Content title={_title} desc={_desc} />
+        <Control
+          onChangeMode={function(_mode) {
+            this.setState({
+              mode: _mode,
+            })
+          }.bind(this)}
+        />
+
+        {_article}
       </div>
     );
   }
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    console.log('TOC rendcer shouldComponentUpdate'
+      , newProps.data
+      , this.props.data);
+
+      if (this.props.data === newProps.data) {
+        return false;
+      }
+      return true;
+  }
+
   render() {
-    console.log('TOC render');
+    console.log('  -> TOC render');
 
     const data = this.props.data;
     const lists = [];
@@ -10,7 +21,15 @@ class TOC extends Component {
 
     while(i < data.length) {
       lists.push(
-        <li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>
+        <li key={data[i].id}>
+          <a
+            href={"/content/"+data[i].id}
+            data-id={data[i].id}
+            onClick={function(e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}>{data[i].title}</a>
+        </li>
       );
       i = i + 1;
     }
